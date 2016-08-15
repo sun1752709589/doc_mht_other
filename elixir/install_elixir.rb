47b934f8fbfc1324eaea7ef0e5344fde46c8d8a1
sun <1752709589@qq.com>
2016-08-15 13:50:58 +0800

    install elixir

---
+++ elixir/install_elixir.rb
@@ -0,0 +1,4 @@
+1: wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && sudo dpkg -i erlang-solutions_1.0_all.deb
+2: sudo apt-get update
+3: sudo apt-get install esl-erlang
+4: sudo apt-get install elixir
